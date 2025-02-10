b37ac531bb2971db866499f655c43a3f48fb8a70
Pooja <poojalawande66@gmail.com>
2025-02-10 21:08:30 +0530

    spark session change

--- com/dell/util/utility.py
+++ com/dell/util/utility.py
@@ -2,4 +2,4 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local[*]").appName("Read File").getOrCreate()
 
-print("Spark Verson :- ", spark.version)
+print("Spark Version :- ", spark.version)
